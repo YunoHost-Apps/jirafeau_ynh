0a1ddc7f33c355bd0df35ddb2dfde03790153102
ericgaspar <junk.eg@free.fr>
2022-08-15 07:14:50 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -7,7 +7,7 @@
 YNH_PHP_VERSION="8.0"
 
 # dependencies used by the app
-pkg_dependencies=""
+pkg_dependencies="php$YNH_PHP_VERSION-fpm"
 
 #=================================================
 # PERSONAL HELPERS
